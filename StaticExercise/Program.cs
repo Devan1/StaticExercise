3e133211390251fb3355f3e2c8fb40f196e1c130
Devan <deshlock010101@hotmai.com>
2022-02-24 09:31:20 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"20 Celsius to Fahrenheit is: {TempConverter.CelsiusToFahrenheit(20)}");
+            Console.WriteLine($"68 Fahrenheit to Celsius is: {TempConverter.FahrenheitToCelsius(68)}");
+        }
+    }
+}
